353de88deeb1540c660c03cb6ba9497d39f35d8b
Adrian Mallari <adriantmallari@gmail.com>
2023-05-30 07:51:01 +0800

    Add files via upload

---
+++ cs3qp.js
@@ -0,0 +1,16 @@
+function summaryActive(){
+	var x = 0;
+	var lva = ["FALCON 9 BLOCK 5", "SOYUZ FG", "ATLAS V N22", "ANTARES"];
+		while(x<4){
+		document.write(lva[x]+"</br>");
+		x++;
+	}		
+}	
+function summaryRetired(){
+	var i = 0;
+	var lvr = ["H-II", "SPACE TRANSPORTATION SYSTEM"];
+		while(i<2){
+		document.write(lvr[i]+"</br>");
+		i++;
+	}	
+}
